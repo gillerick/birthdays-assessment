afec296c392f6bdf12a288c1b97e32ea57041179
Gill Erick <erick@sendyit.com>
2022-06-06 09:03:45 +0300

    TASK 1: Move checkIfBirthday function to a utility file

---
+++ birthdayUtils/birthdayUtils.go
@@ -0,0 +1,25 @@
+package birthdayUtils
+
+import "time"
+
+// CheckIfBirthday /*This method signature is for the purpose of enabling testing.
+//We are deliberately passing the currentDate as opposed to instantiating it within the function
+//The method returns a birthday in the []string format or an error
+//*/
+func CheckIfBirthday(dateOfBirth []string, currentDate time.Time) ([]string, error) {
+	//Here, we are interested in the date of birth which is the last element in our array
+	birthDate, err := time.Parse("2006/02/01", dateOfBirth[len(dateOfBirth)-1])
+
+	if err != nil {
+		return nil, err
+	}
+
+	currentDay := currentDate.Day()
+	currentMonth := currentDate.Month()
+
+	if currentMonth == birthDate.Month() && currentDay == birthDate.Day() {
+		return dateOfBirth, nil
+	} else {
+		return nil, nil
+	}
+}

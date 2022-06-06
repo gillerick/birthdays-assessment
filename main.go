4aaa79557df311a546c0f2f87a545eec7793d636
Gill Erick <erick@sendyit.com>
2022-06-06 08:39:21 +0300

    TASK 1: Read source file & map birthdays to interface

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
-func main()  {
+import (
+	"encoding/json"
+	"io/ioutil"
+)
 
+func main()  {
+	//1. Read birthdays from source file (JSON)
+	birthdaysByte, _ := ioutil.ReadFile("birthdays.json")
+
+	type multiStringArray [][]string
+
+	var birthdaysArray multiStringArray
+
+	if err := json.Unmarshal(birthdaysByte, &birthdaysArray); err != nil{
+		panic(err)
+	}
 }

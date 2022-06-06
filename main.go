c6daaf64f4a7b3bdc48964c7b0751057cac18896
Gill Erick <erick@sendyit.com>
2022-06-06 08:52:33 +0300

    TASK 1: Loop through birthdays and perform a birthday check

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"io/ioutil"
+	"time"
 )
 
 func main()  {
@@ -13,7 +14,26 @@ func main()  {
 
 	var birthdaysArray multiStringArray
 
+	//2. Map the birthdays to the expected interface
 	if err := json.Unmarshal(birthdaysByte, &birthdaysArray); err != nil{
 		panic(err)
 	}
+
+	//Initialize empty array of type multiStringArray for storing today's birthdays
+	todayBirthdays := make(multiStringArray, 0)
+
+	//3. Loop through the mapped values and perform a birthday check
+	for _, birthday := range birthdaysArray {
+		currentDate := time.Now()
+		dateOfBirth, err := time.Parse("2006/01/02", birthday[len(birthday)-1])
+
+		if err != nil{
+			panic(err)
+		}
+
+		if dateOfBirth.Day() == currentDate.Day() && dateOfBirth.Month() == currentDate.Month() {
+			todayBirthdays = append(todayBirthdays, dateOfBirth)
+		}
+	}
+
 }
